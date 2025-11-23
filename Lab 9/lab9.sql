52856b3f00e5b0e1f6b32f92551a3407263271aa
Sanzhar <omarkhanovsanzh@gmail.com>
2025-11-23 17:05:09 +0500

    Lab 9

--- Lab 9/lab9.sql
+++ Lab 9/lab9.sql
@@ -14,7 +14,6 @@ INSERT INTO Reviewer (reviewerID, name) VALUES
 (307, 'Daniel White'),
 (308, 'Sophia Lee');
 
--- 2 --
 CREATE TABLE Movie (
     movieID INT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
@@ -32,7 +31,6 @@ INSERT INTO Movie (movieID, title, releaseYear, director) VALUES
 (407, 'Lost in the Echo', 2024, 'Daniel White'),
 (408, 'Shadow of Destiny', 2024, 'James Clarke');
 
--- 3 --
 CREATE TABLE Review (
     reviewerID INT,
     movieID INT,
@@ -53,7 +51,7 @@ INSERT INTO Review (reviewerID, movieID, rating, reviewDate) VALUES
 (306, 407, 5, '2024-02-05'),
 (307, 408, 4, '2024-03-12');
 
--- 4 --
+-- 2 --
 CREATE OR REPLACE VIEW HighRatedMovieYears AS
 SELECT DISTINCT m.releaseYear
 FROM Movie m
@@ -61,26 +59,26 @@ JOIN Review r ON m.movieID = r.movieID
 WHERE r.rating >= 4
 ORDER BY m.releaseYear ASC;
 
--- 5 --
+-- 3 --
 CREATE INDEX idx_review_rating ON Review(rating);
 CREATE INDEX idx_movie_releaseYear ON Movie(releaseYear);
 
--- 6 --
+-- 4 --
 CREATE ROLE lab_user WITH LOGIN CREATEROLE;
 
--- 7 --
+-- 5 --
 GRANT CONNECT ON DATABASE lab9 TO lab_user;
 
 GRANT USAGE ON SCHEMA public TO lab_user;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO lab_user;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO lab_user;
 
--- 8 --
+-- 6 --
 ALTER TABLE Reviewer OWNER TO lab_user;
 ALTER TABLE Movie OWNER TO lab_user;
 ALTER TABLE Review OWNER TO lab_user;
 
--- 9 --
+-- 7 --
 CREATE OR REPLACE VIEW TopRated2024Movies AS
 SELECT m.title AS movie_title, rv.name AS reviewer_name
 FROM Movie m
